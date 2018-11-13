715c6d66495bc47bf8777c6bc60946000f790d7f
TheoRelativity <marioandrea.petruccelli@gmail.com>
2018-11-13 12:06:31 +0100

    Removed Hacking Template

--- templates/hacking/home_.php
+++
@@ -1,2 +0,0 @@
-<h1>This is the home<h1>
-<?= $template->index() ?>
--- templates/hacking/paper_index.php
+++
@@ -1,8 +0,0 @@
-<h1><?= $this->PAPER['info']['title']?> </h1>
-<?php foreach($this->PAPER['index'] as $obj => $info)
-{
-
-	$html = ""; 
-	$depth=1;
-	echo objToHTML($info,$html,$depth,$obj); 
-}
